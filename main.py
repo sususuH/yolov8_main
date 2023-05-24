2f7661bf275f93bb48786d25d10228ee912b9398
sususuH <623647049@qq.com>
2023-05-24 09:54:38 +0800

    在main添加了train运行指令

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from ultralytics import YOLO
 import time
 
 model = YOLO("/home/su/su/yolo/yolov8_tracking-master_main/yolov8/cfg/yolov8l.yaml")
+# model = YOLO("/home/su/su/yolo/yolov8_tracking-master_yolo/yolov8/model/yolov8l.pt")  # load a pretrained model (recommended for training)
+
+# results = model.train(data="/home/su/su/yolo/yolov8_tracking-master_yolo/yolov8/datapath/score_data.yaml", epochs=3,batch=2,imgsz=640,pretrained=True,optimizer='SGD')  # train the model
+
 # model.predict(source="/home/su/su/yolo/yolov8_tracking-master_main/runs/detect/bad_video_frame/cut1.mp4")
 
 # for n in range(179)
